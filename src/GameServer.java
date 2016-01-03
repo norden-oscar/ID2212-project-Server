e5bcdfe81a3c11baf8a777862a2e2a71799b0adc
norden-oscar <norden.oscar@gmail.com>
2016-01-03 20:51:08 +0100

    end of day

--- src/GameServer.java
+++ src/GameServer.java
@@ -3,20 +3,36 @@ import java.net.Socket;
 public class GameServer implements Runnable {
 
 	private Socket clientSocket1,clientSocket2;
+	protected State state;
 	
-	public GameServer(Socket client1){
-		clientSocket1 = client1;
+	public GameServer(){
+		
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		initializeGame();
+		searchingForPlayers();
+		playingGame();
+		
 		
 	}
+private void playingGame() {
+		// TODO Själva spel logiken och main spel loopen
+		
+	}
+
+private void searchingForPlayers() {
+		// TODO Väntar på 2 spelare,
+		
+	}
+
 private void initializeGame(){
-	
+	// TODO skapar serversocketen och datatstrukturer som behövs under spelets gång
 }
 private void addPlayer(Socket client2){
+	clientSocket2 = client2;
+	searchingForPlayers = false;
 	
 }
 }
