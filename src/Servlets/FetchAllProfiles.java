1fd5c84bada29311bc945a41332a0ba84a73aef8
ankstek <ankstek@live.se>
2016-01-04 17:03:32 +0100

    glassfish shit and servlets and made it into dynamic web project + java project

---
+++ src/Servlets/FetchAllProfiles.java
@@ -0,0 +1,21 @@
+package Servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class FetchAllProfiles extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String userName = request.getParameter("userName");
+		String remoteAddr = request.getRemoteAddr();
+
+		// TODO: send data to server
+
+		response.getWriter().write("OK");
+	}
+}
